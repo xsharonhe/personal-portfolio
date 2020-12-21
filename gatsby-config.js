4008b816ad962f1841b63d62c17fcb286d57a001
xsharonhe <xsharonhe@gmail.com>
2020-12-21 00:31:21 -0500

    Setup theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,6 +56,9 @@ module.exports = {
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components'
+    'gatsby-plugin-styled-components',
+    {
+      resolve: `gatsby-styled-components-dark-mode`
+    }
   ]
 }
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export * from './Container';
+export * from './Header';
+export * from './LayoutMain';
+export * from './LayoutRoot';
+export * from './Page';
